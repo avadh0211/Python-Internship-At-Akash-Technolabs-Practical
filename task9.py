a9a2ad9e7115afc9070d8278c43dad0ef3cca79d
Avadh Patel <85208962+avadh0211@users.noreply.github.com>
2021-06-14 16:56:32 +0530

    Delete task9.py

--- task9.py
+++
@@ -1,9 +0,0 @@
-a = int(input("enter the value : "))
-
-if a<100:
-    if a%2==0:
-        print("the number is even : ")
-    else:
-        print("the number is odd. ")
-else:
-    print("number is greater than 100.")
