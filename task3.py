368c877bf3e162d0c3f8a84e16663ca5423f5df1
Avadh Patel <85208962+avadh0211@users.noreply.github.com>
2021-06-14 16:56:16 +0530

    Delete task3.py

--- task3.py
+++
@@ -1,5 +0,0 @@
-year = int(input("enter the year you want to check : "))
-if (year%4==0 and year%100!=0 or year%400==0):
-    print("this year is leap year  ")
-else:
-    print(" this year is not leap year")    
