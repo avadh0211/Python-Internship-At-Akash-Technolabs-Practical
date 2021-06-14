989d4f11ec1618ed27d1f69f7c7c32cd83e3a83c
Avadh Patel <85208962+avadh0211@users.noreply.github.com>
2021-06-14 16:56:24 +0530

    Delete task8.py

--- task8.py
+++
@@ -1,7 +0,0 @@
-a = int(input("enter first number : "))
-b = int(input("enter second number : "))
-
-if a<b:
-    print("{} is smallest number : ".format(a))
-if b<a:
-    print("{} is smallest number. ".format(b))
